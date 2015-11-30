fcc46b084011d05a7171341b5dea75100b292363
Michael Shigorin <mike@altlinux.org>
2015-12-01 00:18:48 +0300

    Revert "regular.mk: move lxqt to sddm"
    This reverts commit fd1044d540d82944e208d5976123b21a225ab38d:
    there's no sddm in p7/t7 and there likely will be none.

--- conf.d/regular.mk
+++ conf.d/regular.mk
@@ -177,7 +177,7 @@ distro/regular-kde4: distro/.regular-desktop use/x11/kde4/nm use/x11/kdm4 \
 	@$(call add,THE_PACKAGES,volumes-profile-lite)
 	@$(call add,DEFAULT_SERVICES_ENABLE,prefdm)
 
-mixin/regular-lxqt: use/x11/lxqt use/x11/sddm \
+mixin/regular-lxqt: use/x11/lxqt use/x11/lightdm/lxqt \
 	use/net/connman use/browser/qupzilla +plymouth
 	@$(call add,THE_PACKAGES,qconnman-ui)
 
