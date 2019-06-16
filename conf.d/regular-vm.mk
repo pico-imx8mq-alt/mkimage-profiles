d2f5abe74e9b44c9e3572d3a60c3fcbd2d4a1f9d
Anton Midyukov <antohami@altlinux.org>
2019-06-16 16:03:53 +0700

    regular-vm.mk: add mipsel support

--- conf.d/regular-vm.mk
+++ conf.d/regular-vm.mk
@@ -13,9 +13,16 @@ BASE_TARGET += use/bootloader/uboot use/kernel use/firmware
 DESKTOP_TARGET += use/no-sleep use/x11/armsoc
 endif
 
+ifeq (,$(filter-out mipsel,$(ARCH)))
+DESKTOP_TARGET += use/x11/radeon
+endif
+
 vm/.regular-base: $(BASE_TARGET)
 ifneq (,$(filter-out i586 x86_64,$(ARCH)))
 	@$(call add,DEFAULT_SERVICES_DISABLE,multipathd)
+endif
+ifeq (,$(filter-out mipsel,$(ARCH)))
+	@$(call set,KFLAVOURS,un-malta)
 endif
 	@:
 
@@ -39,5 +46,26 @@ vm/regular-lxqt: profile/regular-lxqt vm/.regular-desktop; @:
 vm/regular-mate: profile/regular-mate vm/.regular-desktop; @:
 vm/regular-xfce: profile/regular-xfce vm/.regular-desktop; @:
 vm/regular-icewm: profile/regular-icewm vm/.regular-desktop; @:
+
+
+ifeq (,$(filter-out mipsel,$(ARCH)))
+
+vm/tavolga-regular-lxde: vm/regular-lxde use/mipsel-mitx; @:
+vm/tavolga-regular-lxqt: vm/regular-lxqt use/mipsel-mitx; @:
+vm/tavolga-regular-mate: vm/regular-mate use/mipsel-mitx; @:
+vm/tavolga-regular-xfce: vm/regular-xfce use/mipsel-mitx; @:
+vm/tavolga-regular-icewm: vm/regular-icewm use/mipsel-mitx; @:
+vm/tavolga-regular-jeos-systemd: vm/regular-jeos-systemd use/mipsel-mitx; @:
+vm/tavolga-regular-jeos-sysv: vm/regular-jeos-sysv use/mipsel-mitx; @:
+
+vm/bfk3-regular-lxde: vm/regular-lxde use/mipsel-bfk3; @:
+vm/bfk3-regular-lxqt: vm/regular-lxqt use/mipsel-bfk3; @:
+vm/bfk3-regular-mate: vm/regular-mate use/mipsel-bfk3; @:
+vm/bfk3-regular-xfce: vm/regular-xfce use/mipsel-bfk3; @:
+vm/bfk3-regular-icewm: vm/regular-icewm use/mipsel-bfk3; @:
+vm/bfk3-regular-jeos-systemd: vm/regular-jeos-systemd use/mipsel-bfk3; @:
+vm/bfk3-regular-jeos-sysv: vm/regular-jeos-sysv use/mipsel-bfk3; @:
+
+endif
 
 endif
