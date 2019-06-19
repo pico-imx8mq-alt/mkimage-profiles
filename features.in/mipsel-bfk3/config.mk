f287bd38380a1bbf2db7be4f024fdb176611156e
Anton Midyukov <antohami@altlinux.org>
2019-06-20 02:36:04 +0700

    mipsel-bfk3: set VM_FSTYPE ext2

--- features.in/mipsel-bfk3/config.mk
+++ features.in/mipsel-bfk3/config.mk
@@ -1,6 +1,8 @@
 use/mipsel-bfk3: use/kernel
 	@$(call add_feature)
 	@$(call set,KFLAVOURS,bfk3-def)
+	@$(call set,VM_FSTYPE,ext2)
+	@$(call xport,VM_FSTYPE)
 	@$(call add,NET_ETH,eth0:dhcp)
 	@$(call add,NET_ETH,eth1:dhcp)
 	@$(call add,NET_ETH,eth2:dhcp)
